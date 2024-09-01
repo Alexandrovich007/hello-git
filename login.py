23442f6ea91a70c01d5a4747a0273557041e7a5d
Ruslan <r.tejerinazap@outlook.es>
2024-09-01 13:23:00 +0200

    cambio programa login

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ print ("Hello Git with changes")
 print("Login: modifican el login desde la rama login")
 print("Con borrado incluido")
 
-var = 1
+
 if var == 1:
     print("Esto es un cambio")
 else:
