bfdfb14f1bcc655e3e33c97b0090da51b3caa45d
Ruslan <r.tejerinazap@outlook.es>
2024-09-01 13:33:11 +0200

    borrado codigo

--- login.py
+++ login.py
@@ -2,8 +2,6 @@ print ("Hello Git with changes")
 print("Login: modifican el login desde la rama login")
 print("Con borrado incluido")
 
-var = 1
-if var == 1:
     print("Esto es un cambio")
 else:
     print("Esto no es un cambio")
