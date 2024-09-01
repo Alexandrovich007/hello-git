1e21ce5ce5693bc4cdcc6b9faefed30185e9980d
Ruslan <r.tejerinazap@outlook.es>
2024-09-01 14:24:15 +0200

    cambios importantes programa

--- login.py
+++ login.py
@@ -3,11 +3,7 @@ print("Login: modifican el login desde la rama login")
 print("Con borrado incluido")
 var = 1
 var2 = 2
-if var == 1:
-    print("Esto es un cambio")
-else:
-    print("Este es otro cambio")
-    print("Este es el cambio final")
+
 
 if var2 == 1:
     print("Esto es un cambio")  
