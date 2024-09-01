4a1cf84ae7650987f5feab43111edbed55553a00
Ruslan <r.tejerinazap@outlook.es>
2024-09-01 13:39:59 +0200

    error login linea 4

--- login.py
+++ login.py
@@ -1,8 +1,7 @@
 print ("Hello Git with changes")
 print("Login: modifican el login desde la rama login")
 print("Con borrado incluido")
-variable = 1
-if variable == 1:
+
     print("Esto es un cambio")
 else:
     print("Esto no es un cambio")
