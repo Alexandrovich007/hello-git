2b555dfbce1f306c7d13a3d4fac03be23e40eced
Ruslan <r.tejerinazap@outlook.es>
2024-09-04 11:08:57 +0200

    borrado codigo login.py

--- login.py
+++ login.py
@@ -1,13 +1,2 @@
 print ("Hello Git with changes")
 print("Login: version 2.0")
-
-
-var = 5
-var2 = 2
-
-
-if var2 == 1:
-    print("Esto es un cambio")  
-else:
-    print("Este es otro cambio")
-    print("Este es el cambio final")
